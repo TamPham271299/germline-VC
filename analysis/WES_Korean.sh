dc24834a76c96eacd6c7e98697ae597b162bbe5f
TamPham <phammaitam99@gmail.com>
2025-05-20 22:47:09 +0700

    Update WES_Korean.sh

--- analysis/WES_Korean.sh
+++ analysis/WES_Korean.sh
@@ -1,54 +1,36 @@
 #!bin/bash
 
 proj_n="WES_Korean"
-scripts="/home/mdnl/Documents/Tam_14Mar25/code/WGS_WES/scripts"
 
 export proj_n="$proj_n"
 
-SAMPLE="YUHL937-21"
-cd /home/mdnl/Documents/Tam_14Mar25/MD/variant_calling/WES_Korean
-mkdir -p raw/$SAMPLE/
-mv ~/Downloads/${SAMPLE}_*.fq.gz raw/$SAMPLE/
-
-samples=("YUHL937-21")
+# Specify samples
+# samples=("YUHL937-21")
 for SAMPLE in "${samples[@]}"; do
     echo $SAMPLE
-
-    conda activate trim_galore
-
+    mkdir -p raw/$SAMPLE/
+    
     # 011. QC
-    env SAMPLE="$SAMPLE" bash "$scripts/011_QC_pe.sh"
+    env SAMPLE="$SAMPLE" bash "code/011_QC_pe.sh"
 
     # 012. Trim
-    env SAMPLE="$SAMPLE" bash "$scripts/012_trim_pe.sh"
+    env SAMPLE="$SAMPLE" bash "code/012_trim_pe.sh"
 
-    conda deactivate
-
-    conda activate variant_calling
     # 021. BWA
-    env SAMPLE="$SAMPLE" bash "$scripts/021_bwa_mem2_pe.sh"
+    env SAMPLE="$SAMPLE" bash "code/021_bwa_mem2_pe.sh"
 
     # 031. per-sample variant calling (SNP + short indel)
-    env SAMPLE="$SAMPLE" bash "$scripts/031_snp_indel_calling.sh"
+    env SAMPLE="$SAMPLE" bash "code/031_snp_indel_calling.sh"
 
-    conda deactivate
-
-    # Store processed data in KianaRDS
-    cp -r raw/$SAMPLE/ ~/KianaRDS/Tam_14May25/MD/variant_calling/WES_Korean/raw/
-    cp -r trimmed/$SAMPLE/ ~/KianaRDS/Tam_14May25/MD/variant_calling/WES_Korean/trimmed/
-    cp -r bwa_aligned/$SAMPLE/ ~/KianaRDS/Tam_14May25/MD/variant_calling/WES_Korean/bwa_aligned/
-
-    # # Remove processed data in WS to free up space
-    # rm -r raw/$SAMPLE/ trimmed/$SAMPLE/ bwa_aligned/$SAMPLE/
 done
 
 # 032. Joint calling
-bash "$scripts/032_joint_calling.sh"
+bash "code/032_joint_calling.sh"
 
 # 033. Variant filtering using GATK VQSR machine learning
 snp_cutoff=90.0
 indel_cutoff=90.0
-env proj_n="$proj_n" snp_cutoff="$snp_cutoff" indel_cutoff="$indel_cutoff" bash "$scripts/033_variant_filtering.sh"
+env proj_n="$proj_n" snp_cutoff="$snp_cutoff" indel_cutoff="$indel_cutoff" bash "code/033_variant_filtering.sh"
 
 ## 034. Variant filtering using hard filters (Optional)
 snp_cutoff=90.0
@@ -64,38 +46,32 @@ filter_options_INDEL="-filter \"QD < 2.0\" --filter-name \"QD_2\" \
                     -filter \"FS > 200.0\" --filter-name \"FS_200\" \
                     -filter \"QUAL < 30.0\" --filter-name \"QUAL_30\" \
                     -filter \"ReadPosRankSum < -20.0\" --filter-name \"ReadPosRankSum_-20.0\""
-env proj_n="$proj_n" snp_cutoff="$snp_cutoff" indel_cutoff="$indel_cutoff" filter_options_SNP="$filter_options_SNP" filter_options_INDEL="$filter_options_INDEL" bash "$scripts/034_variant_filtering_hard.sh"
+env proj_n="$proj_n" snp_cutoff="$snp_cutoff" indel_cutoff="$indel_cutoff" filter_options_SNP="$filter_options_SNP" filter_options_INDEL="$filter_options_INDEL" bash "code/034_variant_filtering_hard.sh"
 
 ## 035. Rare/Common variants
 maf=0.05
 
-env proj_n="$proj_n" maf="$maf" bash "$scripts/035_rare_variants.sh" 
+env proj_n="$proj_n" maf="$maf" bash "code/035_rare_variants.sh" 
 
-## 036. Evaluate callset
-inputs=("cohort.indel.90.0.hard_filter.MAF.0.05.vcf" "cohort.indel.90.0.hard_filter.MAF.0.05.rare.vcf" "cohort.snp.90.0.hard_filter.MAF.0.05.vcf" "cohort.snp.90.0.hard_filter.MAF.0.05.rare.vcf")
+# ## 036. Evaluate callset
+# inputs=("cohort.indel.90.0.hard_filter.MAF.0.05.vcf" "cohort.indel.90.0.hard_filter.MAF.0.05.rare.vcf" "cohort.snp.90.0.hard_filter.MAF.0.05.vcf" "cohort.snp.90.0.hard_filter.MAF.0.05.rare.vcf")
 
-for input in "${inputs[@]}"; do
-    echo $input
-    env proj_n="$proj_n" input="$input" bash "$scripts/036_evaluate_callset.sh"
-done
+# for input in "${inputs[@]}"; do
+#     echo $input
+#     env proj_n="$proj_n" input="$input" bash "code/036_evaluate_callset.sh"
+# done
 
 ## 041. Annotation
-conda activate vep
-env proj_n="$proj_n" bash "$scripts/041_vep.sh"
-conda deactivate
+env proj_n="$proj_n" bash "code/041_vep.sh"
 
 ## 042. Filter VEP annotation
-conda activate variant_calling
-env proj_n="$proj_n" bash "$scripts/042_vep_filter.sh"
-conda deactivate
-
+env proj_n="$proj_n" bash "code/042_vep_filter.sh"
 ## 043. Convert VCF into tab_delimited
-env proj_n="$proj_n" bash "$scripts/043_vcf_to_tab.sh"
+env proj_n="$proj_n" bash "code/043_vcf_to_tab.sh"
 
 # 04. Data analysis
-## 043. SVA (MAF < 0.05)
-
-## 044. GBA (MAF < 0.1)
+## 043. SVA (MAF < 0.05) (Run code/051_SVA_GBA.r)
+## 044. GBA (MAF < 0.05) (Run code/051_SVA_GBA.r)
 
 
 
